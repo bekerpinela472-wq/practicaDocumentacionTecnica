716bd5fcffac0a76d1e2f9a9d811415faf4fac5d
BekerPinela <bekerpinela472@gmail.com>
2026-05-21 20:29:16 -0500

    suma de dos numeros

---
+++ app.py
@@ -0,0 +1,8 @@
+# Programa para sumar dos números
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+
+print("La suma es:", suma)
